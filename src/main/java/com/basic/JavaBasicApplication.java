2685461ddcf2ce509dc6f51c992e1bd59baafd9c
yommmm <1529133840@qq.com>
2018-03-31 23:21:00 +0800

    init

---
+++ src/main/java/com/basic/JavaBasicApplication.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBasicApplication.class, args);
+	}
+}
