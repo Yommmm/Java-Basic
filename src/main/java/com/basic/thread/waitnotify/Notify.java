1e67583ceb6fa1f65012e634260655707d0156a2
杨智文 <yangzhiwen@chalco-steering.com>
2019-02-22 14:35:59 +0800

    我想被裁员啊

---
+++ src/main/java/com/basic/thread/waitnotify/Notify.java
@@ -0,0 +1,24 @@
+package com.basic.thread.waitnotify;
+
+public class Notify extends Thread {
+	
+	private Object lock;
+	
+	public Notify(Object lock) {
+		super();
+		this.lock = lock;
+	}
+
+	@Override
+	public void run() {
+		super.run();
+		synchronized (lock) {
+			System.out.println(lock.toString() + Thread.currentThread().getId() + "开始通知 " + System.currentTimeMillis());
+//			lock.notify();
+//			lock.notify();
+			lock.notifyAll();
+			System.out.println(lock.toString() + Thread.currentThread().getId() + "结束通知 " + System.currentTimeMillis());
+		}
+	}
+
+}
---
+++ src/main/java/com/basic/thread/waitnotify/Run.java
@@ -0,0 +1,35 @@
+package com.basic.thread.waitnotify;
+
+public class Run {
+
+	/*public static void main(String[] args) {
+		try {
+			String lock = new String();
+			System.out.println("sync 上面");
+			synchronized(lock) {
+				System.out.println("sync 第一行");
+				lock.wait();
+				System.out.println("sync 后");
+			}
+			System.out.println("sync 下面");
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}*/
+	
+	public static void main(String[] args) {
+		try {
+			String a = new String("a");
+			Wait wait = new Wait(a);
+			Wait wait1 = new Wait(a);
+			wait.start();
+			wait1.start();
+			Thread.sleep(3000);
+			Notify notify = new Notify(a);
+			notify.start();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
