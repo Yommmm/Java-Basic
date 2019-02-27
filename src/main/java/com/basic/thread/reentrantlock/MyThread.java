36bfa134450e96db14375e9857f079ef56b3ac53
杨智文 <yangzhiwen@chalco-steering.com>
2019-02-27 14:46:01 +0800

    reentrantlock可重入锁demo

---
+++ src/main/java/com/basic/thread/reentrantlock/MyThread.java
@@ -0,0 +1,17 @@
+package com.basic.thread.reentrantlock;
+
+public class MyThread extends Thread {
+	
+	private MyService myService;
+	
+	public MyThread(MyService myService) {
+		super();
+		this.myService = myService;
+	}
+
+	@Override
+	public void run() {
+		myService.testMethod();
+	}
+	
+}
---
+++ src/main/java/com/basic/thread/reentrantlock/Run.java
@@ -0,0 +1,21 @@
+package com.basic.thread.reentrantlock;
+
+public class Run {
+
+	public static void main(String[] args) {
+		MyService myService = new MyService();
+		MyThread t1 = new MyThread(myService);
+		MyThread t2 = new MyThread(myService);
+		MyThread t3 = new MyThread(myService);
+		MyThread t4 = new MyThread(myService);
+		MyThread t5 = new MyThread(myService);
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+		t5.start();
+		
+	}
+	
+}
