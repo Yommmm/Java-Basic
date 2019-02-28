9ef4b8df79fdef3ee37647ea12c80fa6b2863d1d
杨智文 <yangzhiwen@chalco-steering.com>
2019-02-28 14:44:03 +0800

    公平锁和非公平锁

---
+++ src/main/java/com/basic/thread/fairandunfair/MyService.java
@@ -0,0 +1,23 @@
+package com.basic.thread.fairandunfair;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class MyService {
+
+	private ReentrantLock lock;
+
+	public MyService(Boolean isFair) {
+		this.lock = new ReentrantLock(isFair);
+	}
+	
+	public void serviceMethod() {
+		try {
+			lock.lock();
+			System.out.println("Lock " + Thread.currentThread().getName() + "获得锁定");
+		} finally {
+			lock.unlock();
+		}
+		
+	}
+	
+}
---
+++ src/main/java/com/basic/thread/fairandunfair/Run.java
@@ -0,0 +1,38 @@
+package com.basic.thread.fairandunfair;
+
+public class Run {
+
+	public static void main(String[] args) {
+		final MyService myService = new MyService(true);
+		
+		final MyService myService1 = new MyService(false);
+		
+		Runnable runnable = new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Fair Start " + Thread.currentThread().getName() + "开始运行");
+				myService.serviceMethod();
+			}
+		};
+		
+		for(int i = 1; i <= 10; i++) {
+			new Thread(runnable).start();
+		}
+		
+		Runnable runnable1 = new Runnable() {
+			
+			@Override
+			public void run() {
+				System.out.println("Unfair Start " + Thread.currentThread().getName() + "开始运行");
+				myService1.serviceMethod();
+			}
+		};
+		
+		for(int i = 1; i <= 10; i++) {
+			new Thread(runnable1).start();
+		}
+		
+	}
+	
+}
