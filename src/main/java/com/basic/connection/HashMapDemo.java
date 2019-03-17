c3088d637fd9cae4b5e1ce924df89d65d91fe05b
yommmm <1529133840@qq.com>
2019-03-17 22:57:54 +0800

    add dir

---
+++ src/main/java/com/basic/connection/HashMapDemo.java
@@ -0,0 +1,14 @@
+package com.basic.connection;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		Map<String, String> map = new HashMap<>();
+		Map<String, String> concurrent = new ConcurrentHashMap<>();
+	}
+	
+}
