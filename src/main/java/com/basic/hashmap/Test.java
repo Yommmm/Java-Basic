2926f2086f3300600317c21c46cbd7c54cf65428
杨智文 <yangzhiwen@chalco-steering.com>
2019-03-18 10:06:58 +0800

    add hashmap demo

---
+++ src/main/java/com/basic/hashmap/Test.java
@@ -0,0 +1,12 @@
+package com.basic.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test {
+
+	public void test() {
+		Map<String, String> map = new HashMap<String, String>();
+	}
+	
+}
