63808ca3f526e0e0cc8ab4d7057f4d3085d790f1
杨智文 <yangzhiwen@chalco-steering.com>
2019-03-19 18:05:31 +0800

    LinkedListStack

---
+++ src/main/java/com/basic/datastruct/stack/LinkedListStack.java
@@ -0,0 +1,30 @@
+package com.basic.datastruct.stack;
+
+public class LinkedListStack<E> implements Stack<E> {
+
+	@Override
+	public void push(E e) {
+		
+	}
+
+	@Override
+	public E pop() {
+		return null;
+	}
+
+	@Override
+	public E peek() {
+		return null;
+	}
+
+	@Override
+	public int getSize() {
+		return 0;
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return false;
+	}
+
+}
