335a77509390541beea9c6d8381afc9c5794ce62
杨智文 <yangzhiwen@chalco-steering.com>
2019-03-25 17:11:17 +0800

    最小堆

---
+++ src/main/java/com/basic/datastruct/heap/PriorityQueue.java
@@ -0,0 +1,38 @@
+package com.basic.datastruct.heap;
+
+import com.basic.datastruct.queue.Queue;
+
+public class PriorityQueue<E extends Comparable<E>> implements Queue<E> {
+
+	private MaxHeap<E> maxHeap;
+	
+	public PriorityQueue() {
+		this.maxHeap = new MaxHeap<>();
+	}
+
+	@Override
+	public void enQueue(E e) {
+		maxHeap.add(e);
+	}
+
+	@Override
+	public E deQueue() {
+		return maxHeap.extractMax();
+	}
+
+	@Override
+	public E getFront() {
+		return maxHeap.findMax();
+	}
+
+	@Override
+	public int getSize() {
+		return maxHeap.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return maxHeap.isEmpty();
+	}
+
+}
---
+++ src/main/java/com/basic/datastruct/heap/PriorityQueueDemo.java
@@ -0,0 +1,17 @@
+package com.basic.datastruct.heap;
+
+public class PriorityQueueDemo {
+
+	public static void main(String[] args) {
+		
+		int i = 1;
+		System.out.println(i++ + ++i);
+	}
+	
+	public void test() {
+		PriorityQueue<Integer> 
+		
+		
+	}
+	
+}
