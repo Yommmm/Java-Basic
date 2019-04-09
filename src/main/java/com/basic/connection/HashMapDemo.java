ed4ef24d93ab388f1c69510ecb2c944beafe6db8
杨智文 <yangzhiwen@chalco-steering.com>
2019-04-09 14:02:43 +0800

    优先队列测试

--- src/main/java/com/basic/connection/HashMapDemo.java
+++ src/main/java/com/basic/connection/HashMapDemo.java
@@ -1,6 +1,7 @@
 package com.basic.connection;
 
 import java.util.HashMap;
+import java.util.Hashtable;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -8,6 +9,7 @@ public class HashMapDemo {
 
 	public static void main(String[] args) {
 		Map<String, String> map = new HashMap<>();
+		Hashtable<Object, Object> hashtable = new Hashtable<>();
 		Map<String, String> concurrent = new ConcurrentHashMap<>();
 	}
 	
--- src/main/java/com/basic/datastruct/heap/PriorityQueueDemo.java
+++ src/main/java/com/basic/datastruct/heap/PriorityQueueDemo.java
@@ -1,16 +1,53 @@
 package com.basic.datastruct.heap;
 
-public class PriorityQueueDemo {
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
+public class PriorityQueueDemo {
+	
 	public static void main(String[] args) {
+		User tom = new User(2, "Tom");
+		User jack = new User(2, "Jack");
+		User tony = new User(3, "Tony");
+		User bob = new User(3, "Bob");
+		User hahahhh = new User(1, "Hahahhh");
 		
-		int i = 1;
-		System.out.println(i++ + ++i);
+		PriorityQueue<User> queue = new PriorityQueue<User>(new UserComparator());
+		
+		queue.add(tom);
+		queue.add(tom);
+		queue.add(jack);
+		queue.add(jack);
+		queue.add(tony);
+		queue.add(tony);
+		queue.add(bob);
+		queue.add(bob);
+		queue.add(hahahhh);
+		
+		while(queue.size() > 0) {
+			User user = queue.poll();
+			System.out.println(user.username);
+		}
 	}
 	
-	public void test() {
-		
-		
+}
+
+class UserComparator implements Comparator<User> {
+
+	@Override
+	public int compare(User o1, User o2) {
+		return o1.priority - o2.priority;
+	}
+	
+}
+
+class User {
+	int priority;
+	String username;
+	
+	public User(int priority, String username) {
+		this.priority = priority;
+		this.username = username;
 	}
 	
 }
