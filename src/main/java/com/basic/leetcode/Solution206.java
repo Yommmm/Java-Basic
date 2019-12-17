5d086267c85dafe042398b5c8e5fd299c4c068e1
杨智文 <yangzhiwen@chalco-steering.com>
2019-12-17 10:25:01 +0800

    solution 92、98、206

---
+++ src/main/java/com/basic/leetcode/Solution206.java
@@ -0,0 +1,58 @@
+package com.basic.leetcode;
+
+import com.basic.datastruct.map.Solution;
+
+/**
+ * 反转一个单链表。
+ *
+ * 示例:
+ *
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ * 进阶:
+ * 你可以迭代或递归地反转链表。你能否用两种方法解决这道题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution206 {
+
+    public static void main(String[] args) {
+        Solution206 solution = new Solution206();
+
+        ListNode listNode = solution.reverseList(ListNode.getList(1, 2, 3, 4, 5));
+        ListNode.printNode(listNode);
+
+        ListNode listNode2 = solution.reverseList1(ListNode.getList(1, 2, 3, 4, 5));
+        ListNode.printNode(listNode2);
+    }
+
+    public ListNode reverseList(ListNode head) {
+        if(null == head || null == head.next) {
+            return head;
+        }
+
+        ListNode prev = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+
+        return prev;
+    }
+
+    public ListNode reverseList1(ListNode head) {
+        ListNode prev = null;
+        ListNode cur = head;
+
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+
+            cur = next;
+        }
+
+        return prev;
+    }
+
+}
--- src/main/java/com/basic/leetcode/Solution92.java
+++ src/main/java/com/basic/leetcode/Solution92.java
@@ -1,16 +1,20 @@
 package com.basic.leetcode;
 
+import com.basic.datastruct.map.Solution;
+
+import java.util.List;
+
 /**
  * 反转从位置 m 到 n 的链表。请使用一趟扫描完成反转。
- *
+ * <p>
  * 说明:
  * 1 ≤ m ≤ n ≤ 链表长度。
- *
+ * <p>
  * 示例:
- *
+ * <p>
  * 输入: 1->2->3->4->5->NULL, m = 2, n = 4
  * 输出: 1->4->3->2->5->NULL
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/reverse-linked-list-ii
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -19,24 +23,51 @@ public class Solution92 {
 
     public static void main(String[] args) {
 
+        Solution92 solution = new Solution92();
+
+        ListNode listNode = ListNode.getList(1, 2, 3, 4, 5);
+        ListNode.printNode(solution.reverseBetween(listNode, 2, 3));
+
+        ListNode listNode2 = ListNode.getList(1, 2, 3, 4, 5);
+        ListNode.printNode(solution.reverseBetween(listNode2, 1, 5));
+
+        ListNode listNode3 = ListNode.getList(1, 2, 3, 4, 5);
+        ListNode.printNode(solution.reverseBetween(listNode3, 4, 5));
     }
 
     public ListNode reverseBetween(ListNode head, int m, int n) {
+        ListNode dummyNode = new ListNode(-1);
+        dummyNode.next = head;
+
+        ListNode prev = dummyNode;
+
         int index = 1;
-        ListNode cur = head;
-        ListNode pre;
-        ListNode end;
-
-        while (cur != null) {
-            if(m == index + 1) {
-                pre = cur;
-            }
-
-            index ++;
-            cur = cur.next;
+        while (index < m) {
+            prev = prev.next;
+            index++;
         }
 
-        return head;
+        prev.next = this.reverseList(prev.next, n - m);
+
+        return dummyNode.next;
+    }
+
+    public ListNode reverseList(ListNode head, int k) {
+        ListNode prev = null;
+        ListNode cur = head;
+
+        int index = 0;
+        while (cur != null && index <= k) {
+            ListNode next = cur.next;
+            cur.next = prev;
+            prev = cur;
+
+            cur = next;
+            index ++;
+        }
+
+        head.next = cur;
+        return prev;
     }
 
 }
