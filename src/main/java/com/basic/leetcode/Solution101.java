8ffbacedf42edc3e28560c75c81b526eba468444
杨智文 <yangzhiwen@chalco-steering.com>
2019-12-24 10:32:36 +0800

    solution 101补充解法

--- src/main/java/com/basic/leetcode/Solution101.java
+++ src/main/java/com/basic/leetcode/Solution101.java
@@ -1,7 +1,32 @@
 package com.basic.leetcode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
- *
+ * 给定一个二叉树，检查它是否是镜像对称的。
+ * <p>
+ * 例如，二叉树 [1,2,2,3,4,4,3] 是对称的。
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * / \ / \
+ * 3  4 4  3
+ * 但是下面这个 [1,2,2,null,3,null,3] 则不是镜像对称的:
+ * <p>
+ * 1
+ * / \
+ * 2   2
+ * \   \
+ * 3    3
+ * 说明:
+ * <p>
+ * 如果你可以运用递归和迭代两种方法解决这个问题，会很加分。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/symmetric-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution101 {
 
@@ -14,10 +39,30 @@ public class Solution101 {
     }
 
     private boolean isSymmetric(TreeNode left, TreeNode right) {
-        if(null == left && null == right) return true;
+        if (null == left && null == right) return true;
 
-        if(null == left || null == right) return false;
+        if (null == left || null == right) return false;
 
         return left.val == right.val && isSymmetric(left.right, right.left) && isSymmetric(left.left, right.right);
     }
+
+    public boolean isSymmetric2(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
+        q.add(root);
+        q.add(root);
+        while (!q.isEmpty()) {
+            TreeNode t1 = q.poll();
+            TreeNode t2 = q.poll();
+            if (t1 == null && t2 == null) continue;
+            if (t1 == null || t2 == null) return false;
+            if (t1.val != t2.val) return false;
+            q.add(t1.left);
+            q.add(t2.right);
+            q.add(t1.right);
+            q.add(t2.left);
+        }
+
+        return true;
+    }
+
 }
