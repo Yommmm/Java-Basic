f3bd01fd1db12b252daa7cc2f9100d25ef51d2e5
Yommmm <853744290@qq.com>
2020-01-02 10:06:20 +0800

    Solution 148，116

---
+++ src/main/java/com/basic/leetcode/Solution116.java
@@ -0,0 +1,91 @@
+package com.basic.leetcode;
+
+/**
+ * 给定一个完美二叉树，其所有叶子节点都在同一层，每个父节点都有两个子节点。二叉树定义如下：
+ * <p>
+ * struct Node {
+ * int val;
+ * Node *left;
+ * Node *right;
+ * Node *next;
+ * }
+ * 填充它的每个 next 指针，让这个指针指向其下一个右侧节点。如果找不到下一个右侧节点，则将 next 指针设置为 NULL。
+ * <p>
+ * 初始状态下，所有 next 指针都被设置为 NULL。
+ * <p>
+ * 提示：
+ * <p>
+ * 你只能使用常量级额外空间。
+ * 使用递归解题也符合要求，本题中递归程序占用的栈空间不算做额外的空间复杂度。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution116 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public Node connect2(Node root) {
+        if (root == null) return null;
+
+        Node level = root;
+        Node cur = level;
+        while (level.left != null) {
+            cur.left.next = cur.right;
+
+            if (cur.next == null) {
+                level = level.left;
+                cur = level;
+            } else {
+                cur.right.next = level.next.left;
+                cur = cur.next;
+            }
+        }
+
+        return root;
+    }
+
+    public Node connect(Node root) {
+        if (root == null) return null;
+
+        if (root.left != null) {
+            root.left.next = root.right;
+
+            if (root.next != null) {
+                root.right.next = root.next.left;
+            }
+        }
+
+        connect(root.left);
+        connect(root.right);
+        return root;
+    }
+
+    // Definition for a Node.
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+}
+
+
--- src/main/java/com/basic/leetcode/Solution148.java
+++ src/main/java/com/basic/leetcode/Solution148.java
@@ -2,16 +2,16 @@ package com.basic.leetcode;
 
 /**
  * 在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: 4->2->1->3
  * 输出: 1->2->3->4
  * 示例 2:
- *
+ * <p>
  * 输入: -1->5->3->4->0
  * 输出: -1->0->3->4->5
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/sort-list
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
@@ -19,24 +19,37 @@ package com.basic.leetcode;
 public class Solution148 {
 
     public static void main(String[] args) {
+        Solution148 solution = new Solution148();
 
+        ListNode list = ListNode.getList(9, 8, 7, 6);
+        ListNode listNode = solution.sortList(list);
+        ListNode.printNode(listNode);
     }
 
+    /**
+     * 归并排序
+     * @param head
+     * @return
+     */
     public ListNode sortList(ListNode head) {
-        if (head == null || head.next == null) return head;
+        if (null == head || null == head.next) return head;
 
-        ListNode fast = head.next, slow = head;
-        while (fast != null && fast.next != null) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while (null != fast && null != fast.next) {
             slow = slow.next;
             fast = fast.next.next;
         }
-        ListNode tmp = slow.next;
+
+        ListNode temp = slow.next;
         slow.next = null;
         ListNode left = sortList(head);
-        ListNode right = sortList(tmp);
+        ListNode right = sortList(temp);
+
         ListNode h = new ListNode(0);
-        ListNode res = h;
-        while (left != null && right != null) {
+        ListNode result = h;
+
+        while (null != left && null != right) {
             if (left.val < right.val) {
                 h.next = left;
                 left = left.next;
@@ -46,8 +59,10 @@ public class Solution148 {
             }
             h = h.next;
         }
-        h.next = left != null ? left : right;
-        return res.next;
+
+        h.next = null == left ? right : left;
+
+        return result.next;
     }
 
 }
