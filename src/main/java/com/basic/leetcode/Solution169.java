36d995cc91f3013610bd1f1ed0cc77f93455fa10
Yommmm <853744290@qq.com>
2020-01-10 10:48:49 +0800

    solution 139、169、172、191、Trie

---
+++ src/main/java/com/basic/leetcode/Solution169.java
@@ -0,0 +1,42 @@
+package com.basic.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个大小为 n 的数组，找到其中的多数元素。多数元素是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+ *
+ * 你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+ *
+ * 示例 1:
+ *
+ * 输入: [3,2,3]
+ * 输出: 3
+ * 示例 2:
+ *
+ * 输入: [2,2,1,1,1,2,2]
+ * 输出: 2
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/majority-element
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution169 {
+
+    public static void main(String[] args) {
+        Solution169 solution = new Solution169();
+        System.out.println(solution.majorityElement(new int[]{6, 5, 5}));
+    }
+
+    public int majorityElement(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+            if (map.getOrDefault(num, 0) > nums.length >> 1) return num;
+        }
+
+        return 0;
+    }
+
+}
---
+++ src/main/java/com/basic/leetcode/Solution172.java
@@ -0,0 +1,47 @@
+package com.basic.leetcode;
+
+import com.basic.datastruct.map.Solution;
+
+/**
+ * 给定一个整数 n，返回 n! 结果尾数中零的数量。
+ *
+ * 示例 1:
+ *
+ * 输入: 3
+ * 输出: 0
+ * 解释: 3! = 6, 尾数中没有零。
+ * 示例 2:
+ *
+ * 输入: 5
+ * 输出: 1
+ * 解释: 5! = 120, 尾数中有 1 个零.
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/factorial-trailing-zeroes
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution172 {
+
+    public static void main(String[] args) {
+        Solution172 solution = new Solution172();
+        System.out.println(solution.trailingZeroes(3));
+        System.out.println(solution.trailingZeroes(5));
+        System.out.println(solution.trailingZeroes(25));
+    }
+
+    /**
+     * 一个5就对应一个0
+     * @param n
+     * @return
+     */
+    public int trailingZeroes(int n) {
+        int count = 0;
+
+        while (n > 0) {
+            count += (n = n / 5);
+        }
+
+        return count;
+    }
+
+}
