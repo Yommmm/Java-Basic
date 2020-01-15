79c49c901faff633506d150e4d4c93f52311e06c
Yommmm <853744290@qq.com>
2020-01-15 22:08:31 +0800

    solution 143,236,560

---
+++ src/main/java/com/basic/leetcode/Solution143.java
@@ -0,0 +1,58 @@
+package com.basic.leetcode;
+
+import java.util.List;
+import java.util.Stack;
+
+/**
+ *
+ */
+public class Solution143 {
+
+    public static void main(String[] args) {
+        Solution143 solution = new Solution143();
+        ListNode list = ListNode.getList(1, 2);
+        solution.reorderList(list);
+    }
+
+    public void reorderList(ListNode head) {
+        Stack<ListNode> stack = new Stack<>();
+
+        ListNode cur = head;
+        while (null != cur) {
+            stack.push(cur);
+            cur = cur.next;
+        }
+
+        if (stack.size() < 3) return;
+
+        ListNode c = head;
+        for (int i = 0; i < stack.size(); i++) {
+            ListNode pop = stack.pop();
+            pop.next = c.next;
+            c.next = pop;
+            c = pop.next;
+        }
+
+        c.next = null;
+        ListNode.printNode(head);
+    }
+
+    public void reorderList1(ListNode head) {
+        ListNode cur = head;
+        while(null != cur) {
+            cur.next = reverse(cur.next);
+            cur = cur.next;
+        }
+    }
+
+    private ListNode reverse(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode last = reverse(head.next);
+        head.next.next = head;
+        head.next = null;
+        return last;
+    }
+
+}
---
+++ src/main/java/com/basic/leetcode/Solution560.java
@@ -0,0 +1,61 @@
+package com.basic.leetcode;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * 给定一个整数数组和一个整数 k，你需要找到该数组中和为 k 的连续的子数组的个数。
+ * <p>
+ * 示例 1 :
+ * <p>
+ * 输入:nums = [1,1,1], k = 2
+ * 输出: 2 , [1,1] 与 [1,1] 为两种不同的情况。
+ * 说明 :
+ * <p>
+ * 数组的长度为 [1, 20,000]。
+ * 数组中元素的范围是 [-1000, 1000] ，且整数 k 的范围是 [-1e7, 1e7]。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/subarray-sum-equals-k
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution560 {
+
+    public static void main(String[] args) {
+//        Solution560 solution = new Solution560();
+//        System.out.println(solution.subarraySum(new int[]{1, 1, 1}, 2));
+        List<String> strings = Arrays.asList("1,2,3,4".split(","));
+        System.out.println(strings.size());
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        int count = 0;
+
+        for (int start = 0; start < nums.length; start++) {
+            for (int end = start + 1; end <= nums.length; end++) {
+                int sum = 0;
+                for (int i = start; i < end; i++) {
+                    sum += nums[i];
+                    if (sum == k) count++;
+                }
+            }
+        }
+
+        return count;
+    }
+
+    public int subarraySum1(int[] nums, int k) {
+        int count = 0, sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, 1);
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (map.containsKey(sum - k))
+                count += map.get(sum - k);
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return count;
+    }
+
+}
