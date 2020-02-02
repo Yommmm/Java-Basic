e86b1bc6e96b853356409a99f28847e1863ce5df
Yommmm <853744290@qq.com>
2020-02-02 11:37:35 +0800

    NumMatrix, 345

---
+++ src/main/java/com/basic/leetcode/NumMatrix.java
@@ -0,0 +1,79 @@
+package com.basic.leetcode;
+
+/**
+ * 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2)。
+ * <p>
+ * <p>
+ * 上图子矩阵左上角 (row1, col1) = (2, 1) ，右下角(row2, col2) = (4, 3)，该子矩形内元素的总和为 8。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 matrix = [
+ * [3, 0, 1, 4, 2],
+ * [5, 6, 3, 2, 1],
+ * [1, 2, 0, 1, 5],
+ * [4, 1, 0, 1, 7],
+ * [1, 0, 3, 0, 5]
+ * ]
+ * <p>
+ * sumRegion(2, 1, 4, 3) -> 8
+ * sumRegion(1, 1, 2, 2) -> 11
+ * sumRegion(1, 2, 2, 4) -> 12
+ * 说明:
+ * <p>
+ * 你可以假设矩阵不可变。
+ * 会多次调用 sumRegion 方法。
+ * 你可以假设 row1 ≤ row2 且 col1 ≤ col2。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/range-sum-query-2d-immutable
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class NumMatrix {
+
+    /*private int[][] matrix;
+
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for(int i = row1; i <= row2; i++) {
+            for(int j = col1; j <= col2; j++) {
+                sum += this.matrix[i][j];
+            }
+        }
+        return sum;
+    }*/
+
+    private int[][] dp;
+
+    public NumMatrix(int[][] matrix) {
+        if (matrix.length == 0 || matrix[0].length == 0) return;
+
+        dp = new int[matrix.length][matrix[0].length + 1];
+        for(int r = 0; r < matrix.length; r++) {
+            for(int c = 0; c < matrix[0].length; c++) {
+                dp[r][c + 1] = matrix[r][c] + dp[r][c];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+
+        for(int r = row1; r <= row2; r++) {
+            sum += dp[r][col2 + 1] - dp[r][col1];
+        }
+
+        return sum;
+    }
+
+}
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix obj = new NumMatrix(matrix);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
+ */
---
+++ src/main/java/com/basic/leetcode/Solution345.java
@@ -0,0 +1,63 @@
+package com.basic.leetcode;
+
+/**
+ * 编写一个函数，以字符串作为输入，反转该字符串中的元音字母。
+ *
+ * 示例 1:
+ *
+ * 输入: "hello"
+ * 输出: "holle"
+ * 示例 2:
+ *
+ * 输入: "leetcode"
+ * 输出: "leotcede"
+ * 说明:
+ * 元音字母不包含字母"y"。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-vowels-of-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution345 {
+
+    public static void main(String[] args) {
+        Solution345 solution = new Solution345();
+        System.out.println(solution.reverseVowels("hello"));
+        System.out.println(solution.reverseVowels("leetcode"));
+    }
+
+    public String reverseVowels(String s) {
+        char[] chars = s.toCharArray();
+
+        int left = 0;
+        int right = s.length() - 1;
+
+        while (left < right) {
+            if(!is(chars[left])) {
+                left++;
+                continue;
+            }
+            if(!is(chars[right])) {
+                right--;
+                continue;
+            }
+            swap(chars, left, right);
+            left++;
+            right--;
+        }
+
+        return String.valueOf(chars);
+    }
+
+    private void swap(char[] chars, int left, int right) {
+        char temp = chars[left];
+        chars[left] = chars[right];
+        chars[right] = temp;
+    }
+
+    private boolean is(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+                || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U';
+    }
+
+}
