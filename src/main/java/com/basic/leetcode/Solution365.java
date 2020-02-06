f7907abd1f426d894ad3598d1bcd151d0e3e4669
Yommmm <853744290@qq.com>
2020-02-06 19:17:09 +0800

    Solution 365, 374, 384, 386

---
+++ src/main/java/com/basic/leetcode/Solution365.java
@@ -0,0 +1,44 @@
+package com.basic.leetcode;
+
+/**
+ * 有两个容量分别为 x升 和 y升 的水壶以及无限多的水。请判断能否通过使用这两个水壶，从而可以得到恰好 z升 的水？
+ *
+ * 如果可以，最后请用以上水壶中的一或两个来盛放取得的 z升 水。
+ *
+ * 你允许：
+ *
+ * 装满任意一个水壶
+ * 清空任意一个水壶
+ * 从一个水壶向另外一个水壶倒水，直到装满或者倒空
+ * 示例 1: (From the famous "Die Hard" example)
+ *
+ * 输入: x = 3, y = 5, z = 4
+ * 输出: True
+ * 示例 2:
+ *
+ * 输入: x = 2, y = 6, z = 5
+ * 输出: False
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/water-and-jug-problem
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution365 {
+
+    public static void main(String[] args) {
+        Solution365 solution = new Solution365();
+        System.out.println(solution.canMeasureWater(5, 7, 14));
+    }
+
+    public boolean canMeasureWater(int x, int y, int z) {
+        if(x == 0 && y == 0) return z == 0;
+        return z == 0 || (z % gcd(x,y) == 0 && x + y >= z);
+    }
+
+    private int gcd(int x,int y){
+        if(y == 0) return x;
+        int r = x % y;
+        return gcd(y,r);
+    }
+
+}
---
+++ src/main/java/com/basic/leetcode/Solution384.java
@@ -0,0 +1,72 @@
+package com.basic.leetcode;
+
+import java.util.Random;
+
+/**
+ * 打乱一个没有重复元素的数组。
+ *
+ * 示例:
+ *
+ * // 以数字集合 1, 2 和 3 初始化数组。
+ * int[] nums = {1,2,3};
+ * Solution solution = new Solution(nums);
+ *
+ * // 打乱数组 [1,2,3] 并返回结果。任何 [1,2,3]的排列返回的概率应该相同。
+ * solution.shuffle();
+ *
+ * // 重设数组到它的初始状态[1,2,3]。
+ * solution.reset();
+ *
+ * // 随机返回数组[1,2,3]打乱后的结果。
+ * solution.shuffle();
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/shuffle-an-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution384 {
+
+    class Solution {
+        private int[] array;
+        private int[] original;
+        Random rand = new Random();
+
+        public Solution(int[] nums) {
+            array = nums;
+            original = nums.clone();
+        }
+
+        /** Resets the array to its original configuration and return it. */
+        public int[] reset() {
+            array = original;
+            original = original.clone();
+            return original;
+        }
+
+        /** Returns a random shuffling of the array. */
+        public int[] shuffle() {
+            for (int i = 0; i < array.length; i++) {
+                swapAt(i, randRange(i, array.length));
+            }
+            return array;
+        }
+
+        private int randRange(int min, int max) {
+            return rand.nextInt(max - min) + min;
+        }
+
+        private void swapAt(int i, int j) {
+            int temp = array[i];
+            array[i] = array[j];
+            array[j] = temp;
+        }
+
+    }
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution obj = new Solution(nums);
+ * int[] param_1 = obj.reset();
+ * int[] param_2 = obj.shuffle();
+ */
+}
