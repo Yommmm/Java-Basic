e6d59f7c104ac92b13e5734601b25fd5b205cf7c
Yommmm <853744290@qq.com>
2020-02-24 09:34:32 +0800

    Solution 463，468，478

---
+++ src/main/java/com/basic/leetcode/Solution463.java
@@ -0,0 +1,44 @@
+package com.basic.leetcode;
+
+/**
+ * 给定一个包含 0 和 1 的二维网格地图，其中 1 表示陆地 0 表示水域。
+ *
+ * 网格中的格子水平和垂直方向相连（对角线方向不相连）。整个网格被水完全包围，但其中恰好有一个岛屿（或者说，一个或多个表示陆地的格子相连组成的岛屿）。
+ *
+ * 岛屿中没有“湖”（“湖” 指水域在岛屿内部且不和岛屿周围的水相连）。格子是边长为 1 的正方形。网格为长方形，且宽度和高度均不超过 100 。计算这个岛屿的周长。
+ *
+ *  
+ *
+ * 示例 :
+ *
+ * 输入:
+ * [[0,1,0,0],
+ *  [1,1,1,0],
+ *  [0,1,0,0],
+ *  [1,1,0,0]]
+ *
+ * 输出: 16
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/island-perimeter
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution463 {
+
+    public int islandPerimeter(int[][] grid) {
+        int result = 0;
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[y].length; x++) {
+                if (grid[y][x] == 1) {
+                    if (y - 1 < 0 || grid[y - 1][x] == 0) result++;
+                    if (y + 1 >= grid.length || grid[y + 1][x] == 0) result++;
+                    if (x - 1 < 0 || grid[y][x - 1] == 0) result++;
+                    if (x + 1 >= grid[y].length || grid[y][x + 1] == 0) result++;
+                }
+            }
+        }
+        return result;
+    }
+
+}
