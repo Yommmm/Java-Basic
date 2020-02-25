bf5c47a06cb774122677ca155223d98c43273753
杨智文 <yangzhiwen@chalco-steering.com>
2020-02-26 00:04:26 +0800

    Solution 463，468，478

--- src/main/java/com/basic/leetcode/Solution343.java
+++ src/main/java/com/basic/leetcode/Solution343.java
@@ -2,22 +2,36 @@ package com.basic.leetcode;
 
 /**
  * 给定一个正整数 n，将其拆分为至少两个正整数的和，并使这些整数的乘积最大化。 返回你可以获得的最大乘积。
- *
+ * <p>
  * 示例 1:
- *
+ * <p>
  * 输入: 2
  * 输出: 1
  * 解释: 2 = 1 + 1, 1 × 1 = 1。
  * 示例 2:
- *
+ * <p>
  * 输入: 10
  * 输出: 36
  * 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36。
  * 说明: 你可以假设 n 不小于 2 且不大于 58。
- *
+ * <p>
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode-cn.com/problems/integer-break
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution343 {
+
+    public static void main(String[] args) {
+        Solution343 solution = new Solution343();
+        System.out.println(solution.integerBreak(9));
+    }
+
+    public int integerBreak(int n) {
+        if (n <= 3) return n - 1;
+        int a = n / 3, b = n % 3;
+        if (b == 0) return (int) Math.pow(3, a);
+        if (b == 1) return (int) Math.pow(3, a - 1) * 4;
+        return (int) Math.pow(3, a) * 2;
+    }
+
 }
---
+++ src/main/java/com/basic/leetcode/Solution396.java
@@ -0,0 +1,60 @@
+package com.basic.leetcode;
+
+/**
+ * 给定一个长度为 n 的整数数组 A 。
+ *
+ * 假设 Bk 是数组 A 顺时针旋转 k 个位置后的数组，我们定义 A 的“旋转函数” F 为：
+ *
+ * F(k) = 0 * Bk[0] + 1 * Bk[1] + ... + (n-1) * Bk[n-1]。
+ *
+ * 计算F(0), F(1), ..., F(n-1)中的最大值。
+ *
+ * 注意:
+ * 可以认为 n 的值小于 105。
+ *
+ * 示例:
+ *
+ * A = [4, 3, 2, 6]
+ *
+ * F(0) = (0 * 4) + (1 * 3) + (2 * 2) + (3 * 6) = 0 + 3 + 4 + 18 = 25
+ * F(1) = (0 * 6) + (1 * 4) + (2 * 3) + (3 * 2) = 0 + 4 + 6 + 6 = 16
+ * F(2) = (0 * 2) + (1 * 6) + (2 * 4) + (3 * 3) = 0 + 6 + 8 + 9 = 23
+ * F(3) = (0 * 3) + (1 * 2) + (2 * 6) + (3 * 4) = 0 + 2 + 12 + 12 = 26
+ *
+ * 所以 F(0), F(1), F(2), F(3) 中的最大值是 F(3) = 26 。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/rotate-function
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution396 {
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 用F(k+1) - f(k)可以得到递推公式
+     * F(k+1) = F(k) + sum(A) - nBk[n-1]
+     * @param A
+     * @return
+     */
+    public int maxRotateFunction(int[] A) {
+        int sum = 0;
+        int dp1 = 0, dp2 = 0;
+        for (int i = 0; i < A.length; i++) {
+            sum += A[i];
+            dp1 += i * A[i];
+        }
+
+        int ans = dp1;
+        for (int i = 1; i < A.length; i++) {
+            dp2 = dp1 + sum - A.length * A[A.length - i];
+            ans = Math.max(dp2, ans);
+            dp1 = dp2;
+        }
+
+        return ans;
+    }
+
+}
