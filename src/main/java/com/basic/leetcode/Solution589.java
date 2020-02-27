83f0439a20541c814ccba5156dd8ac7b114ba435
杨智文 <yangzhiwen@chalco-steering.com>
2020-02-27 22:34:17 +0800

    Solution 589

---
+++ src/main/java/com/basic/leetcode/Solution589.java
@@ -0,0 +1,61 @@
+package com.basic.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个 N 叉树，返回其节点值的前序遍历。
+ *
+ * 例如，给定一个 3叉树 :
+ *
+ *  
+ *
+ *
+ *
+ *  
+ *
+ * 返回其前序遍历: [1,3,5,6,2,4]。
+ *
+ *  
+ *
+ * 说明: 递归法很简单，你可以使用迭代法完成此题吗?
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution589 {
+
+    public List<Integer> preorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        this.preorder(root, result);
+
+        return result;
+    }
+
+    private void preorder(Node node, List<Integer> result) {
+        if(null == node) return;
+
+        result.add(node.val);
+        for(Node children : node.children) {
+            this.preorder(children, result);
+        }
+    }
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+}
