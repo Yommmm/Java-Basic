044ef3f26316a383cf25222a3664ec47e2400264
杨智文 <yangzhiwen@chalco-steering.com>
2020-02-28 00:56:01 +0800

    Solution 589

---
+++ src/main/java/com/basic/leetcode/Solution590.java
@@ -0,0 +1,61 @@
+package com.basic.leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 给定一个 N 叉树，返回其节点值的后序遍历。
+ *
+ * 例如，给定一个 3叉树 :
+ *
+ *  
+ *
+ *
+ *
+ *  
+ *
+ * 返回其后序遍历: [5,6,3,2,4,1].
+ *
+ *  
+ *
+ * 说明: 递归法很简单，你可以使用迭代法完成此题吗?
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/n-ary-tree-postorder-traversal
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution590 {
+
+    public List<Integer> postorder(Node root) {
+        List<Integer> result = new ArrayList<>();
+        this.postorder(root, result);
+
+        return result;
+    }
+
+    private void postorder(Node node, List<Integer> result) {
+        if (null == node) return;
+
+        for (Node children : node.children) {
+            this.postorder(children, result);
+        }
+        result.add(node.val);
+    }
+
+    class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {}
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    };
+
+}
---
+++ src/main/java/com/basic/leetcode/Solution606.java
@@ -0,0 +1,50 @@
+package com.basic.leetcode;
+
+/**
+ * 空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: 二叉树: [1,2,3,4]
+ * 1
+ * /   \
+ * 2     3
+ * /
+ * 4
+ * <p>
+ * 输出: "1(2(4))(3)"
+ * <p>
+ * 解释: 原本将是“1(2(4)())(3())”，
+ * 在你省略所有不必要的空括号对之后，
+ * 它将是“1(2(4))(3)”。
+ * 示例 2:
+ * <p>
+ * 输入: 二叉树: [1,2,3,null,4]
+ * 1
+ * /   \
+ * 2     3
+ * \
+ * 4
+ * <p>
+ * 输出: "1(2()(4))(3)"
+ * <p>
+ * 解释: 和第一个示例相似，
+ * 除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/construct-string-from-binary-tree
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution606 {
+
+    public String tree2str(TreeNode t) {
+        if (t == null)
+            return "";
+        if (t.left == null && t.right == null)
+            return t.val + "";
+        if (t.right == null)
+            return t.val + "(" + tree2str(t.left) + ")";
+        return t.val + "(" + tree2str(t.left) + ")(" + tree2str(t.right) + ")";
+    }
+
+}
